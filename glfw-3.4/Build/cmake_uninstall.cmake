559ec588e597f2104c504c6f020054ac3f0d3127
JOE ORCHARD <JO278870@falmouth.ac.uk>
2024-10-10 17:17:03 +0100

    Generated project with Cmake

---
+++ glfw-3.4/Build/cmake_uninstall.cmake
@@ -0,0 +1,29 @@
+
+if (NOT EXISTS "D:/Comp305/glfw-3.4/Build/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"D:/Comp305/glfw-3.4/Build/install_manifest.txt\"")
+endif()
+
+file(READ "D:/Comp305/glfw-3.4/Build/install_manifest.txt" files)
+string(REGEX REPLACE "\n" ";" files "${files}")
+
+foreach (file ${files})
+  message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
+  if (EXISTS "$ENV{DESTDIR}${file}")
+    exec_program("C:/Users/JO278870/Downloads/cmake-3.30.5-windows-x86_64/cmake-3.30.5-windows-x86_64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
+    endif()
+  elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
+    EXEC_PROGRAM("C:/Users/JO278870/Downloads/cmake-3.30.5-windows-x86_64/cmake-3.30.5-windows-x86_64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+                 OUTPUT_VARIABLE rm_out
+                 RETURN_VALUE rm_retval)
+    if (NOT "${rm_retval}" STREQUAL 0)
+      message(FATAL_ERROR "Problem when removing symlink \"$ENV{DESTDIR}${file}\"")
+    endif()
+  else()
+    message(STATUS "File \"$ENV{DESTDIR}${file}\" does not exist.")
+  endif()
+endforeach()
+
