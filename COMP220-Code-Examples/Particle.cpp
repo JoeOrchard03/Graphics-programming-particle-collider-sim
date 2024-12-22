c0b8b2c24d5b2406782bef7533dae34decd03b95
Joe Orchard <JO278870@falmouth.ac.uk>
2024-12-22 14:54:47 +0000

    added the particle code

--- COMP220-Code-Examples/Particle.cpp
+++ COMP220-Code-Examples/Particle.cpp
@@ -1,1 +1,31 @@
 #include "Particle.h"
+
+class Particle {
+public:
+	glm::vec2 position;
+	glm::vec2 previous_position;
+	glm::vec2 acceleration;
+
+	Particle(float x, float y) : position(x, y), previous_position(x, y), acceleration(0, 0) {}
+
+	void applyForce(const glm::vec2& force)
+	{
+		acceleration += force;
+	}
+
+	void update(float timeStep)
+	{
+		glm::vec2 velocity = position - previous_position;
+		previous_position = position;
+		position += velocity + acceleration * timeStep * timeStep;
+		acceleration = glm::vec2(0, 0);
+	}
+
+	void constrainToBounds(float width, float height)
+	{
+		if (position.x < 0) position.x = 0;
+		if (position.x > width) position.x = width;
+		if (position.y < 0) position.y = 0;
+		if (position.y > height) position.y = height;
+	}
+};
--- COMP220-Code-Examples/Particle.h
+++ COMP220-Code-Examples/Particle.h
@@ -1,8 +1,22 @@
 #pragma once
-class Particle
-{
-	void Draw()
-};
+#include <gl\glew.h>
+#include <SDL_opengl.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "Vertex.h"
+#include <assimp/Importer.hpp>	
+#include <assimp/scene.h>	
+#include <assimp/postprocess.h>	
+#include <iostream>
+#include <SDL.h>
+#include <gl\glew.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 
