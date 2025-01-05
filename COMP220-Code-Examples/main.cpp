10df205dfeafafa0474ff6f510a91cc1df39adb0
Joe Orchard <JO278870@falmouth.ac.uk>
2025-01-05 02:35:22 +0000

    Revert "Commit test"
    This reverts commit 84e141e8a650a1cae95e7c2b747db7aa78c85cc9.

--- COMP220-Code-Examples/main.cpp
+++ COMP220-Code-Examples/main.cpp
@@ -575,6 +575,7 @@ int main(int argc, char ** argsv)
 
 	SDL_FreeSurface(image);
 	SDL_GL_DeleteContext(glContext);
+
 	glDeleteProgram(shaderProgram);
 	SDL_GL_DeleteContext(glContext);
 	//Destroy the window and quit SDL2, NB we should do this after all cleanup in this order!!!
